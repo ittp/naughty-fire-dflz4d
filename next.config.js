a26143321c10ce675cf4586e18a6fb2ca1f48125
ittp <61617660+ittp@users.noreply.github.com>
2023-08-05 17:18:24 +0000

    Update 4 files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+
+
+
+const nextConfig = {
+
+    async rewrites() {
+        return [
+          {
+            source: '/about',
+            destination: '/',
+          },
+        ]
+      },
+    assetPrefix: {
+
+    }
+}
 
 module.exports = nextConfig
