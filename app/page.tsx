07f118c32fb69b932ec540a19b0c6706488ccb50
ittp <61617660+ittp@users.noreply.github.com>
2023-08-05 16:47:34 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+  return (<><main
+    className="flex min-h-screen flex-row items-center justify-beetween p-24">
+    <Image 
+          
+      src={"https://support.evotor.ru/assets/images/logo.svg"} 
+      width={150} 
+      height={24}
+      alt={"E"} />
+
+    <input className="pt-2 pb-2 pl-3 pr-3 m-0 br-0 outline-0 text-black rounded-3" type={"search"} />
+    </main>
+
+  </>
+    );
 }
